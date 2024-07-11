5728eda40a22365d1f8bd08ee60b2c07890e97c5
Dima K <@users.noreply.github.com>
2024-07-11 14:43:06 +0200

    Solved data interaction with code challenge

--- queries.py
+++ queries.py
@@ -1,27 +1,93 @@
 # pylint: disable=missing-docstring, C0103
 
+import sqlite3
+
+
 def directors_count(db):
-    # return the number of directors contained in the database
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT count(id)
+    FROM directors d
+    """
+    conn = sqlite3.connect('data/movies.sqlite')
+    db = conn.cursor()
+    db.execute(query)
+    results = db.fetchall()[0][0]
+    print(type(results))
+
+    return results
 
 
 def directors_list(db):
-    # return the list of all the directors sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT name
+    FROM directors d
+    ORDER BY name ASC
+    """
+    conn = sqlite3.connect('data/movies.sqlite')
+    db = conn.cursor()
+    db.execute(query)
+    temp = db.fetchall()
+    results = []
+    for i in temp:
+        results.append(i[0])
+    print(results)
 
+    return results
 
 def love_movies(db):
-    # return the list of all movies which contain the exact word "love"
-    # in their title, sorted in alphabetical order
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT title
+    FROM movies m
+    WHERE
+	LOWER(title) like "% love %"
+	or LOWER(title) like "love %"
+	or LOWER(title) like "% love"
+	or LOWER(title) like "love"
+	or LOWER(title) like "% love'%"
+	or LOWER(title) like "% love."
+	or LOWER(title) like "love,%"
+	or LOWER(title) like " love'%"
+
+    ORDER BY title ASC
+    """
+    conn = sqlite3.connect('data/movies.sqlite')
+    db = conn.cursor()
+    db.execute(query)
+    temp = db.fetchall()
+    results = []
+    for i in temp:
+        results.append(i[0])
+    return results
 
 
 def directors_named_like_count(db, name):
-    # return the number of directors which contain a given word in their name
-    pass  # YOUR CODE HERE
-
+    query = """
+    SELECT COUNT(name)
+    FROM directors d
+    WHERE LOWER(name) LIKE ?
+    """
+    conn = sqlite3.connect('data/movies.sqlite')
+    db = conn.cursor()
+    db.execute(query,(f"%{name.lower()}%",))
+    results = db.fetchone()[0]
+    print(results)
+    return results
 
 def movies_longer_than(db, min_length):
     # return this list of all movies which are longer than a given duration,
     # sorted in the alphabetical order
-    pass  # YOUR CODE HERE
+    query = """
+    SELECT title
+    FROM movies m
+    WHERE minutes > ?
+    ORDER BY title ASC
+    """
+    conn = sqlite3.connect('data/movies.sqlite')
+    db = conn.cursor()
+    db.execute(query,(min_length,))
+    temp = db.fetchall()
+    results = []
+    for i in temp:
+        results.append(i[0])
+    print(results)
+    return results
